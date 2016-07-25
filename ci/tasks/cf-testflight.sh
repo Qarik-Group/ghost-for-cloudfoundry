1ea3c124db870e2b120d8e52206a6fc2cd832117
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-25 14:33:41 +1000

    deploy ghost after update

---
+++ ci/tasks/cf-testflight.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+cf login -a https://api.run.pivotal.io -u ${cf_username} -p ${cf_password} -o ${cf_org} -s ${cf_space}
+set +x
+
+cd ghost-updated
+
+app_name=${app_name:-ghost}
+pg_name=${pg_name:-test-pg}
+s3_name=${s3_name:-test-s3}
+
+cf d ${app_name} -f || echo "skipping..."
+cf ds ${pg_name} -f || echo "skipping..."
+cf ds ${s3_name} -f || echo "skipping..."
+
+cf cs ${cf_pg_service} ${cf_pg_service_plan} ${pg_name}
+cf cs ${cf_s3_service} ${cf_s3_service_plan} ${s3_name}
+
+cf push ${app_name} --random-route --no-start
+
+cf bs ${app_name} ${pg_name}
+cf bs ${app_name} ${s3_name}
+
+cf restart ${app_name}
