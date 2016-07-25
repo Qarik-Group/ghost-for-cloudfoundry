ec0f963459a7156fd3d2ce9ba92a892330a15076
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-25 12:37:32 +1000

    include nodejs/npm

--- ci/tasks/update-ghost-version.sh
+++ ci/tasks/update-ghost-version.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+tar xfz ghost/source.tar.gz
+# creates TryGhost-Ghost-XXXX folder
+
 git clone ghost-for-cf ghost-updated
 
 rm -rf ghost-updated/core ghost-updated/index.js ghost-updated/package.json ghost-updated/shrinkwrap.json
 
+cp -r TryGhost-Ghost-*/core ghost-updated/
+cp -r TryGhost-Ghost-*/index.js ghost-updated/
+cp -r TryGhost-Ghost-*/package.json ghost-updated/
 
-ls -al ghost/
-pushd ghost
-tar xfz source.tar.gz
-ls -al .
+cd ghost-updated
+
+npm install --save
+npm shrinkwrap
