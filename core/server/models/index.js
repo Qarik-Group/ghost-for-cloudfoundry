b23162e90605ebf896b3a1ab372ee8c8e0c0ec46
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-25 11:34:25 +1000

    upgrade to ghost v0.8 https://github.com/TryGhost/Ghost/releases/tag/0.8.0

--- core/server/models/index.js
+++ core/server/models/index.js
@@ -2,8 +2,7 @@
  * Dependencies
  */
 
-var Promise = require('bluebird'),
-    _ = require('lodash'),
+var _ = require('lodash'),
 
     exports,
     models;
@@ -26,6 +25,7 @@ models = [
     'refreshtoken',
     'role',
     'settings',
+    'subscriber',
     'tag',
     'user'
 ];
@@ -36,31 +36,7 @@ function init() {
     models.forEach(function (name) {
         _.extend(exports, require('./' + name));
     });
-
-    return Promise.resolve();
 }
-
-/**
- * TODO: move to some other place
- */
-
-// ### deleteAllContent
-// Delete all content from the database (posts, tags, tags_posts)
-exports.deleteAllContent = function deleteAllContent() {
-    var self = this;
-
-    return self.Post.findAll().then(function then(posts) {
-        return Promise.all(_.map(posts.toJSON(), function mapper(post) {
-            return self.Post.destroy({id: post.id});
-        }));
-    }).then(function () {
-        return self.Tag.findAll().then(function then(tags) {
-            return Promise.all(_.map(tags.toJSON(), function mapper(tag) {
-                return self.Tag.destroy({id: tag.id});
-            }));
-        });
-    });
-};
 
 /**
  * Expose `init`
