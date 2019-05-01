e7548881d4fedb67e0414bc700a2c9427b0dbbcb
Dr Nic Williams <drnicwilliams@gmail.com>
2019-05-02 09:54:09 +1000

    postinstall is performed before we've setup current/ symlink

--- bin/upgrade_ghost.sh
+++ bin/upgrade_ghost.sh
@@ -31,7 +31,7 @@ pushd current
 popd
 
 cp current/package.json .
-sed -i -e 's% core\/server\/public\/% current/core/server/public/%' package.json
+sed -i -e 's%^"postinstall".*$%"postinstall": ""%' package.json
 
 cp current/yarn.lock .
 rm -rf node_modules
